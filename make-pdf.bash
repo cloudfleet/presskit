a82e145f9a07d1e3ffef50a9502a3dbc12df5ddb
Mark <evenson@panix.com>
2016-01-11 07:46:03 +0100

    Conversion to PDF via pandoc

---
+++ make-pdf.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Requirements
+# 1.  Pandoc <http://pandoc.org/installing.html>
+# 2.  TeX Live <https://www.tug.org/texlive/>
+
+for file in *.md; do
+    out=$(basename $file .md)
+    pandoc $file -o "$out".pdf
+done
+
+tmp=all.md
+cat README.md DESCRIPTION.md MENTIONS.md > $tmp
+pandoc $tmp -o cloudfleet-presskit.pdf
+rm $tmp
