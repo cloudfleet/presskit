878b9ccc9becad0add6b610b6733e27e4e65757d
Dražen Lučanin <kermit666@gmail.com>
2016-01-14 14:48:44 +0100

    don't generate individual pdfs

--- make-pdf.bash
+++ make-pdf.bash
@@ -4,12 +4,19 @@
 # 1.  Pandoc <http://pandoc.org/installing.html>
 # 2.  TeX Live <https://www.tug.org/texlive/>
 
-for file in *.md; do
-    out=$(basename $file .md)
-    pandoc $file -o "$out".pdf
-done
+# Running
+#     bash make-pdf.bash
+
+# for now we will only use the single pdf, but with this we could generate
+# individual pdfs
+
+# for file in *.md; do
+#     out=$(basename $file .md)
+#     pandoc $file -o "$out".pdf
+# done
 
 tmp=all.md
-cat README.md DESCRIPTION.md MENTIONS.md media-resources.md > $tmp
+cat README.md DESCRIPTION.md MENTIONS.md media-resources.md \
+  sample-tech-article.md > $tmp
 pandoc $tmp -o cloudfleet-presskit.pdf
 rm $tmp
